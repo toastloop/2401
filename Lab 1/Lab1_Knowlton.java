df961362b4c0aa56f6d8b1481a165830ce51b55c
toastloop <69254179+toastloop@users.noreply.github.com>
2022-08-31 10:19:47 -0600

    Rearranged exceptions so it doesn't calculate the student into the average.

--- Lab 1/Lab1_Knowlton.java
+++ Lab 1/Lab1_Knowlton.java
@@ -17,15 +17,23 @@ public class Lab1_Knowlton{
             File inputFile = null;
             Scanner inputScanner = null;
 
-            // Try/Catch Block to Catch any Exceptions Thrown.
+            // Try to open a file and scanner
             try{
                 // Open the file resource for the given input file
-                inputFile = new File("Lab1_Inputfile2_Knowlton.txt");
+                inputFile = new File("Lab1_Inputfile1_Knowlton.txt");
                 // Opens a scanner to scan through the given input file
                 inputScanner = new Scanner(inputFile);
             }
+
+            // Catch any exceptions
             catch(Exception e){
+
+                // Print the Exception Message
                 System.out.println(e.getMessage());
+
+                // Exit the program with a non-zero code
+                System.exit(1);
+
             }
 
             
@@ -37,8 +45,8 @@ public class Lab1_Knowlton{
             int thirdMidtermSum = 0;
             int thirdMidtermNum = 0;
 
-            // Holds the student number, increases on each loop
-            int studentNum =  0;
+            // Holds the student (row) number, increases on each loop
+            int studentNum =  1;
 
             // Loop while the file scanner has the next line of the file.
             while(inputScanner.hasNextLine()){
@@ -53,66 +61,80 @@ public class Lab1_Knowlton{
                 // Holds the assignment number (column)
                 int assignmentNum = 0;
 
-                // Loop while the line scanner has the next integer in the line.
-                while(lineScanner.hasNextInt()){
+                try{
 
-                    try{
-                    // Read the grade into an integer variable
-                    int grade = lineScanner.nextInt();
+                    // Loop while the line scanner has the next integer in the line.
+                    while(lineScanner.hasNextInt()){
 
-                    // Throw an Exception if it encounters a Negative Number
-                    if(grade < 0){
-                        throw new Exception("Grade must not include a negative number");
+                        // Read the grade into an integer variable
+                        int grade = lineScanner.nextInt();
+
+                        // Throw an Exception if it encounters a Negative Number
+                        if(grade < 0){
+                            throw new Exception("Student " + studentNum + " has a negative grade.");
+                        }
+                        // If it's not a negative number continue to storing the grade
+                        else {
+                        
+                            // Switch based on the assignment number (column)
+                            switch(assignmentNum){
+
+                                // First Midterm Grade
+                                case  0:
+                                    firstMidtermSum +=  grade;
+                                    firstMidtermNum++;
+                                break;
+
+                                //  Second Midterm Grade
+                                case  1:
+                                    secondMidtermSum +=  grade;
+                                    secondMidtermNum++;
+                                break;
+
+                                // Third Midterm Grade
+                                case  2:
+                                    thirdMidtermSum +=  grade;
+                                    thirdMidtermNum++;
+                                break;
+
+                                // Default value
+                                default:
+                                    throw new Exception("Number of grades exceeds number of midterms");
+
+                            }
+
+                            // Check if the grade is greater than  the maximum grade so far.
+                            if(grade > maxGrade){
+                                // Assign maximum  grade to the current grade.
+                                maxGrade = grade;
+                            }
+
+                        }
+
+                        // Increase the assignment number (column) counter.
+                        assignmentNum++;
+
                     }
-                    
 
-                    // Switch based on the assignment number (column)
-                    switch(assignmentNum){
+                    // Close the scanner for the line
+                    lineScanner.close();
 
-                        // First Midterm Grade
-                        case  0:
-                            firstMidtermSum +=  grade;
-                            firstMidtermNum++;
-                        break;
+                    if(assignmentNum < 3){
 
-                        //  Second Midterm Grade
-                        case  1:
-                            secondMidtermSum +=  grade;
-                            secondMidtermNum++;
-                        break;
+                        // Throw an exception stating that the student has missing grades.
+                        throw new Exception("Student " + studentNum + " has missing grades.");
 
-                        // Third Midterm Grade
-                        case  2:
-                            thirdMidtermSum +=  grade;
-                            thirdMidtermNum++;
-                        break;
+                    } else {
 
-                        // Default value
-                        default:
-                            throw new Exception("Number of grades exceeds number of midterms");
+                        //  Print the student number (row) and the highest grade in the row
+                        System.out.println("Student " + studentNum + " highest score: " + maxGrade);
 
-                    }
-
-                    // Check if the grade is greater than  the maximum grade so far.
-                    if(grade > maxGrade){
-                        // Assign maximum  grade to the current grade.
-                        maxGrade = grade;
-                    }
-
-                    // Increase the assignment number (column) counter.
-                    assignmentNum++;
-                    }
-                    catch(Exception  e){
-                        System.out.println(e.getMessage());
                     }
 
                 }
-
-                //  Print the student number (row) and the highest grade in the row
-                System.out.println("Student " + studentNum + " highest score: " + maxGrade);
-
-                // Close the line scanner
-                lineScanner.close();
+                catch(Exception  e){
+                    System.out.println(e.getMessage());
+                }
 
                 // Increase the student number (row) counter.
                 studentNum++;
