84b8fdbf75b38502cd742c594041f2f9ed9b128e
toastloop <69254179+toastloop@users.noreply.github.com>
2022-08-31 09:16:36 -0600

    Added Lab1 Files

---
+++ Lab1_Knowlton.java
@@ -0,0 +1,130 @@
+/*
+    Name: Lab1_Knowlton
+    Author Name: Matthew Knowlton
+    Class: CS2401 Elementary Data Structures and Algorithms
+    CRN: 18418
+    Lab Number: 1
+    Date: 30 August 2022
+*/
+
+// Import File and Scanner libraries
+import java.io.File;
+import java.util.Scanner;
+
+public class Lab1_Knowlton{
+    public static void main(String[] args){
+
+            File inputFile = null;
+            Scanner inputScanner = null;
+
+            // Try/Catch Block to Catch any Exceptions Thrown.
+            try{
+                // Open the file resource for the given input file
+                inputFile = new File("Lab1_Inputfile2_Knowlton.txt");
+                // Opens a scanner to scan through the given input file
+                inputScanner = new Scanner(inputFile);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+
+            
+            // Holds the sum and number of grades of each midterm (column) in order to calculate average.
+            int firstMidtermSum = 0;
+            int firstMidtermNum = 0;
+            int secondMidtermSum = 0;
+            int secondMidtermNum = 0;
+            int thirdMidtermSum = 0;
+            int thirdMidtermNum = 0;
+
+            // Holds the student number, increases on each loop
+            int studentNum =  0;
+
+            // Loop while the file scanner has the next line of the file.
+            while(inputScanner.hasNextLine()){
+
+                // Read the line of the file into a string variable
+                String line = inputScanner.nextLine();
+                // Open a scanner to scan  through the given line of the file.
+                Scanner lineScanner = new Scanner(line);
+
+                // Holds the maximum grade from the student (row)
+                int maxGrade = 0;
+                // Holds the assignment number (column)
+                int assignmentNum = 0;
+
+                // Loop while the line scanner has the next integer in the line.
+                while(lineScanner.hasNextInt()){
+
+                    try{
+                    // Read the grade into an integer variable
+                    int grade = lineScanner.nextInt();
+
+                    // Throw an Exception if it encounters a Negative Number
+                    if(grade < 0){
+                        throw new Exception("Grade must not include a negative number");
+                    }
+                    
+
+                    // Switch based on the assignment number (column)
+                    switch(assignmentNum){
+
+                        // First Midterm Grade
+                        case  0:
+                            firstMidtermSum +=  grade;
+                            firstMidtermNum++;
+                        break;
+
+                        //  Second Midterm Grade
+                        case  1:
+                            secondMidtermSum +=  grade;
+                            secondMidtermNum++;
+                        break;
+
+                        // Third Midterm Grade
+                        case  2:
+                            thirdMidtermSum +=  grade;
+                            thirdMidtermNum++;
+                        break;
+
+                        // Default value
+                        default:
+                            throw new Exception("Number of grades exceeds number of midterms");
+
+                    }
+
+                    // Check if the grade is greater than  the maximum grade so far.
+                    if(grade > maxGrade){
+                        // Assign maximum  grade to the current grade.
+                        maxGrade = grade;
+                    }
+
+                    // Increase the assignment number (column) counter.
+                    assignmentNum++;
+                    }
+                    catch(Exception  e){
+                        System.out.println(e.getMessage());
+                    }
+
+                }
+
+                //  Print the student number (row) and the highest grade in the row
+                System.out.println("Student " + studentNum + " highest score: " + maxGrade);
+
+                // Close the line scanner
+                lineScanner.close();
+
+                // Increase the student number (row) counter.
+                studentNum++;
+            }
+
+            // close the scanner for the input file.
+            inputScanner.close();
+
+            // Print out the average grade for each midterm (column)
+            System.out.println("Average of the first midterm: " + (firstMidtermSum / firstMidtermNum));
+            System.out.println("Average of the second midterm: " + (secondMidtermSum / secondMidtermNum));
+            System.out.print("Average of the third midterm: " + (thirdMidtermSum / thirdMidtermNum));
+
+    }
+}
