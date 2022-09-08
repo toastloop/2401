3f8d85271f913d1bc780bea066c33b3bb0aacc95
toastloop <69254179+toastloop@users.noreply.github.com>
2022-09-08 00:11:55 -0600

    Create function to open file, and prompt user file

--- Lab 2/Lab2_Knowlton.java
+++ Lab 2/Lab2_Knowlton.java
@@ -10,6 +10,8 @@
 // import the scanner and file libraries
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
 
 public class Lab2_Knowlton{
 
@@ -19,33 +21,104 @@ public class Lab2_Knowlton{
     public static boolean correct = false;
 
     public static void main(String[] args) {
-
-        // stores the resource for the input file.
-        File file = null;
-        // stores the scanner for the input file.
-        Scanner fileScanner = null;
         // stores the file contents in a 12x12 2d-array
         int[][] fileContents = new int[12][12];
+        // Create a scanner for input from user.
+        Scanner inputScanner = new Scanner(System.in);
 
-        // wrapped file and scanner variables to catch file exceptions
+        // loops until the user provides a file that exists
         try{
-
-            // initialize  the  file and fileScanner.
-            file = new File("Lab2_Inputfile2_Knowlton.txt");
-            fileScanner = new Scanner(file);
-
+            fileContents = readFile("Lab2_Inputfile1_Knowlton.txt");
+        } catch(FileNotFoundException e) {
+            System.out.println();
         }
-        // catch file  and scanner related exceptions
-        catch(Exception e){
 
-            // print the error message and exit with a non-zero code.
-            System.out.println(e.getMessage());
-            System.exit(1);
+        /*
+        // holds a boolean if the file exists
+        boolean fileExists = false;
 
-        }
+        do{
+            // ask for file name
+            System.out.println("\nPlease input a file name to check:");
+            System.out.print(">> ");
+            // store file name input
+            String fileName = inputScanner.next();
+            // try to open the given file name
+            try{
+                fileContents = readFile(fileName);
+                fileExists = true;
+            } 
+            // catch the file not found exception
+            catch(FileNotFoundException e){
+                // print out an error telling the user the file doesn't exist
+                System.out.println("\n Error: File '" + fileName + "' was not found");
+                // mark the file does not exist
+                fileExists = false;
+            }
+        } while(fileExists != true);
+        */
+
+        // a variable to hold user selected row
+        int rowNum = 0;
+        // check if user selected row is outside the number of rows.
+        do{
+            // ask the user for the row number to check
+            System.out.println("\nPlease input a row to check: (0-11)");
+            System.out.print(">> ");
+            // store the number in the rowNum
+            rowNum = inputScanner.nextInt();
+            // print an error if the user selected a row outside of the number of rows
+            if(rowNum > fileContents.length-1){
+                System.out.println("\n Error: Ensure your row number is between 0 and 11");
+            }
+        }while(rowNum > fileContents.length-1);
+
+        // a variable to hold user selected column
+        int colNum = 0;
+        // check if user selected column is outside the number of columns.
+        do{
+            // ask the user for the column number to check
+            System.out.println("\nPlease input a column to check: (0-11)");
+            System.out.print(">> ");
+            // store the number in colNum
+            colNum = inputScanner.nextInt();
+            // print an error if the user selected a column outside the number of columns
+            if(colNum > fileContents[0].length-1){
+                System.out.println("\n Error: Ensure your column number is between 0 and 11");
+            }
+        } while(colNum > fileContents[0].length-1);
+
+        System.out.println();
+
+        // Close the user input scanner.
+        inputScanner.close();
+
+        // check the correctness of the array
+        checkArray(fileContents);
+        // check the correctness of the selected row
+        checkRow(fileContents, rowNum);
+        // check the correctness of the selected column
+        checkColumn(fileContents, colNum);
+        // print the triangle form of the array.
+        printTriangle(fileContents);
+
+        System.out.println();
+
+    }
+
+    public static int[][] readFile(String fileName) throws FileNotFoundException{
+
+        // Finds the absolute file of the fileName given
+        String filePath = Path.of(fileName).toAbsolutePath().toString();
+        // Opens the file using the absolute file path
+        File file = new File(filePath);
+        // Opens a scanner using the file resource
+        Scanner fileScanner = new Scanner(file);
 
         // stores the current row number of the file
         int row = 0;
+        // create a 2d array to hold the file contents
+        int[][] fileContents = new int[12][12];
 
         // loop  through each line of the file using fileScanner
         while(fileScanner.hasNextLine()){
@@ -54,6 +127,7 @@ public class Lab2_Knowlton{
             String line = fileScanner.nextLine();
             // create a scanner to parse through the line  we just stored.
             Scanner lineScanner = new Scanner(line);
+
             // col (int) stores the current column of the row.
             int col = 0;
 
@@ -74,41 +148,7 @@ public class Lab2_Knowlton{
         // close the file scanner.
         fileScanner.close();
 
-        // Create a scanner for input from user.
-        Scanner inputScanner = new Scanner(System.in);
-
-        // a variable to hold user selected row
-        int rowNum = -1;
-        // check if user selected row is outside the number of rows.
-        while(rowNum > fileContents.length-1 || rowNum < 0){
-            // ask the user for the row number to check
-            System.out.println("Please input a row to check");
-            // store the number in the rowNum
-            rowNum = inputScanner.nextInt();
-        }
-
-        // a variable to hold user selected column
-        int colNum = -1;
-        // check if user selected column is outside the number of columns.
-        while(colNum > fileContents[0].length-1 || colNum < 0){
-            // ask the user for the column number to check
-            System.out.println("Please input a column to check");
-            // store the number in colNum
-            colNum = inputScanner.nextInt();
-        }
-
-        // Close the user input scanner.
-        inputScanner.close();
-
-        // check the correctness of the array
-        checkArray(fileContents);
-        // check the correctness of the selected row
-        checkRow(fileContents, rowNum);
-        // check the correctness of the selected column
-        checkColumn(fileContents, colNum);
-        // print the triangle form of the array.
-        printTriangle(fileContents);
-
+        return fileContents;
     }
 
     // generates a 12x12 times table as a 2d integer array
