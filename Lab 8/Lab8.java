2273cca13fb53a0da4064522dd0371cfcdc42620
toastloop <69254179+toastloop@users.noreply.github.com>
2022-11-06 19:59:35 -0700

    Working mergeSort

--- Lab 8/Lab8.java
+++ Lab 8/Lab8.java
@@ -77,21 +77,27 @@ public class Lab8 {
    * @param theBoxes
    */
   static void bubbleSort(Box[] theBoxes) {
+    // Setting the boolean value of swap to false.
     boolean swap;
+    // This is a for loop that is going through the array of Box objects and comparing the values of
+    // theBoxes[i] and theBoxes[j] and setting the min value to the smaller value.
     for (int i = 0; i < theBoxes.length - 1; i++) {
+      // reset swap to false
       swap = false;
       for (int j = 0; j < theBoxes.length - i - 1; j++) {
         if(theBoxes[j].compareTo(theBoxes[j + 1]) == 1){
           Box temp = theBoxes[j];
           theBoxes[j] = theBoxes[j + 1];
           theBoxes[j + 1] = temp;
+          // Set swap to true, since we swapped an item
           swap = true;
         }
       }
+      // If we haven't swapped any items, break the loop
       if(swap == false) break;
     }
   }
-  
+
   /**
    * Change the body of this method to arrange the Box
    * objects in the array parameter in ascending order of
@@ -100,11 +106,17 @@ public class Lab8 {
    * @param theBoxes
    */  
   static void selectionSort(Box[] theBoxes) {
+    // This is a for loop that is going through the array of Box objects and comparing the values of
+    // theBoxes[i] and theBoxes[j] and setting the min value to the smaller value.
     for (int i = 0; i < theBoxes.length; i++) {
+      // Setting the min value to the current index of the array.
       int min = i;
+      // This is comparing the values of theBoxes[i] and theBoxes[j] and setting the min value to the smaller
+      // value.
       for (int j = i + 1; j < theBoxes.length; j++) {
         if(theBoxes[min].compareTo(theBoxes[j]) == 1) min = j;
       }
+      // Swapping the values of theBoxes[i] and theBoxes[min]
       Box temp = theBoxes[i];
       theBoxes[i] = theBoxes[min];
       theBoxes[min] = temp;
@@ -119,49 +131,84 @@ public class Lab8 {
    * @param theBoxes
    */    
   static void mergeSort(Box[] theBoxes) {
-    mergeSort(theBoxes, 0, theBoxes.length);
-  }
-  public static void mergeSort(Box[] theBoxes, int left, int right){
-    int middle = (left+right)/2;
-    if(left == right) return;
-    mergeSort(theBoxes, left, middle);
-    mergeSort(theBoxes , middle + 1, right);
-    mergeArray(theBoxes, left, middle, right);
+    mergeSort(theBoxes, 0, theBoxes.length - 1);
   }
 
-  public static void mergeArray(Box[] theBoxes, int left, int middle, int right){
-    Box[] leftSide = new Box[middle - left + 1];
-    Box[] rightSide = new Box[right - middle];
-    // populate left and right arrays;
-    for(int i = 0; i < ((leftSide.length > rightSide.length) ? leftSide.length : rightSide.length); i++){
-      if(i < leftSide.length) leftSide[i] = theBoxes[left + i];
-      if(i < rightSide.length) rightSide[i] = theBoxes[middle + i];
-    }
-    int j = 0, l = 0, r = 0;
-    for (; l < leftSide.length && r < rightSide.length; j++) {
-      if(leftSide[l].compareTo(rightSide[r]) == 1){
-        theBoxes[j] = leftSide[l];
+  /**
+   * If the left position is less than the right position, set the middle of the array, call the method
+   * using the left side of the array, call the method using the right side of the array, and merge the
+   * two sorted sides of the array together
+   * 
+   * @param theBoxes the array of Box objects
+   * @param left the left side of the array
+   * @param right the right side of the array
+   */
+  public static void mergeSort(Box[] theBoxes, int left, int right) {
+      // Checking to see if the left side of the array is less than the right side of the array.
+      if (left < right) {
+          // Finding the middle of the array.
+          int middle = left + (right - left) / 2;
+          // Calling the mergeSort method using the left side of the array.
+          mergeSort(theBoxes, left, middle);
+          // Calling the mergeSort method using the right side of the array.
+          mergeSort(theBoxes, middle + 1, right);
+          // Calling the mergeArray method using the left, middle, and right indexes of the array.
+          mergeArray(theBoxes, left, middle, right);
+      }
+  }
+
+  /**
+   * The mergeArray function takes in an array of Boxes, the left and right indexes, and the middle
+   * index. It creates two arrays, one for the left side and one for the right side. It then fills the
+   * left and right side arrays with the values from theBoxes. It then compares the values in the left
+   * and right side arrays and puts the smaller value into theBoxes. It then fills theBoxes with the
+   * rest of the values in the left and right side arrays
+   * 
+   * @param theBoxes the array of Box objects to be sorted
+   * @param left the left most index of the array
+   * @param middle the middle of the array
+   * @param right the right most index of the array
+   */
+  public static void mergeArray(Box[] theBoxes, int left, int middle, int right) {
+
+      // Creating two arrays, one for the left side and one for the right side.
+      Box[] leftSide = new Box[middle - left + 1];
+      Box[] rightSide = new Box[right - middle];
+
+      // Creating two arrays, one for the left side and one for the right side. It then fills the
+      // left and right side arrays with the values from theBoxes.
+      for (int i = 0; i < leftSide.length; i++) leftSide[i] = theBoxes[left + i];
+      for (int j = 0; j < rightSide.length; j++) rightSide[j] = theBoxes[middle + 1 + j];
+
+
+      // Setting the left, right positions to 0 and boxes to left pointer.
+      int l = 0, r = 0, b = left;
+      
+      // Comparing the values in the left and right side arrays and putting the smaller value into theBoxes.
+      while (l < leftSide.length && r < rightSide.length) {
+          if (leftSide[l].compareTo(rightSide[r]) <= 0) {
+            theBoxes[b] = leftSide[l];
+            l++;
+          }
+          else {
+            theBoxes[b] = rightSide[r];
+            r++;
+          }
+          b++;
+      }
+
+      // This is filling the boxes array with the rest of the left and right array.
+      while (l < leftSide.length) {
+        theBoxes[b] = leftSide[l];
         l++;
+        b++;
       }
-      else{
-        theBoxes[j] = rightSide[r];
+      while (r < rightSide.length) {
+        theBoxes[b] = rightSide[r];
         r++;
+        b++;
       }
-    }
-
-    while(l < leftSide.length){
-      theBoxes[j] = leftSide[l];
-      j++;
-      l++;
-    }
-
-    while(r < rightSide.length){
-      theBoxes[j] = rightSide[r];
-      j++;
-      r++;
-    }
   }
-  
 
   /**
    * Display width, length, height, volume of
@@ -175,7 +222,12 @@ public class Lab8 {
     }
   } 
 
-  
+  /**
+   * It reads a file, creates a Box array, and returns the array
+   * 
+   * @param fileName The name of the file that contains the data.
+   * @return An array of Box objects.
+   */
   static Box[] getBoxArrayFromDataFile (String fileName){
 
     File file = new File(fileName);
